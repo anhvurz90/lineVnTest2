eba9158c685e978d8d4ed155a28b11953df3a037
anhvurz90 <anhvurz90@gmail.com>
2017-09-08 16:45:09 +0700

    set total_chat & latest_chat to user only when chat and user have the same location

--- MiniTest/src/main/resources/updateDb.sql
+++ MiniTest/src/main/resources/updateDb.sql
@@ -1,6 +1,6 @@
 ALTER TABLE user ADD COLUMN total_chat int, ADD COLUMN latest_chat varchar(255) DEFAULT NULL;
-UPDATE user u SET total_chat = (SELECT COUNT(*) FROM chat c WHERE c.status in (1,2) AND c.owner_id = u.id);
-UPDATE user u SET latest_chat = (SELECT title FROM chat c WHERE c.status in (1,2) AND c.owner_id = u.id order by c.id limit 1);
+UPDATE user u SET total_chat = (SELECT COUNT(*) FROM chat c WHERE c.status in (1,2) AND c.owner_id = u.id and c.location_id = u.location_id);
+UPDATE user u SET latest_chat = (SELECT title FROM chat c WHERE c.status in (1,2) AND c.owner_id = u.id and c.location_id = u.location_id order by c.id limit 1);
 
 CREATE INDEX idx_user_loc_id ON user(location_id);
 CREATE INDEX idx_user_name ON user(username);
